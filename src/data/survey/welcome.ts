6b4f843aa7e3d31caf021f226430eb7fbd34c3db
Johan Hallberg <johan.hallberg@svt.se>
2025-04-28 23:46:53 +0200

    10-15

--- src/data/survey/welcome.ts
+++ src/data/survey/welcome.ts
@@ -3,7 +3,7 @@ import { SurveyData } from '@/types/survey';
 
 export const welcomeQuestions: SurveyData = {
   welcome: {
-    message: "Hej och välkommen till vår undersökning om matvanor! Vi är intresserade av att förstå hur du och din familj hanterar mat i vardagen. Dina svar hjälper oss att förstå verkliga utmaningar och behov. Undersökningen tar cirka 5-10 minuter. Vill du börja?",
+    message: "Hej och välkommen till vår undersökning om matvanor! Vi är intresserade av att förstå hur du och din familj hanterar mat i vardagen. Dina svar hjälper oss att förstå verkliga utmaningar och behov. Undersökningen tar cirka 10-15 minuter. Vill du börja?",
     type: "single_choice",
     options: ["Ja, jag vill delta", "Nej tack"],
     next_ja_jag_vill_delta: "intro",
